3b6815f07401641131ff47a254b211c6775b00f1
Christopher Weems <hello@christopherweems.com>
2020-07-03 14:59:37 -0400

    Refactor to use `Data` instead of `Array<Bool>` to store filter data

--- Sources/Bloomer/BloomFilter.swift
+++ Sources/Bloomer/BloomFilter.swift
@@ -4,27 +4,39 @@
 import Foundation
 
 public struct BloomFilter<T: Hashable> {
-    private var data: [Bool]
+    private var data: Data
     private var seeds: [Int]
     
     public mutating func add(_ value: T) {
         self.hashes(for: value)
-            .map { abs($0 % data.count) }
-            .forEach { data[$0] = true }
+            .map { abs($0 % bitCount) }
+            .forEach { hashValue in
+                let index = hashValue / 8
+                let match: UInt8 = 1 << UInt8(hashValue % 8)
+                
+                self.data[index] |= match
+            }
+        
     }
     
     public func contains(_ value: T) -> Bool {
         self.hashes(for: value)
-            .map { abs($0 % data.count) }
-            .allSatisfy { data[$0] == true }
+            .map { abs($0 % bitCount) }
+            .allSatisfy { hashValue in
+                let index = hashValue / 8
+                let match: UInt8 = 1 << UInt8(hashValue % 8)
+                
+                return (data[index] & match) != 0
+            }
     }
     
     // likely to be deprecated in near future
+    // (size is specified in number of bits)
     public init(size: Int, hashCount: Int) {
         assert(0 < size)
         assert(0 < hashCount)
         
-        data = Array(repeating: false, count: size)
+        data = Data(count: max(size / 8, 1))
         seeds = (0..<hashCount).map { _ in Int.random(in: 0..<Int.max) }
         
     }
@@ -39,5 +51,11 @@ fileprivate extension BloomFilter {
             hasher.combine(value)
             return hasher.finalize()
         }
+    }
+}
+
+fileprivate extension BloomFilter {
+    var bitCount: Int {
+        data.count * 8
     }
 }
