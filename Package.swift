e8271bb455f15edb2929e21d18e6840e59d4df13
Christopher Weems <hello@christopherweems.com>
2020-07-03 18:15:17 -0400

    Update Package.swift to depend on unstandard branch 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/christopherweems/unstandard.git", .branch("main")),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .branch("5.3")),
         
     ],
     targets: [
