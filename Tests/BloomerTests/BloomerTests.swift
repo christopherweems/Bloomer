02c80d29ff7d976b3d61b86602a0b9fece199048
Christopher Weems <hello@christopherweems.com>
2020-07-03 15:12:11 -0400

    Add BloomerTests.testMany()

--- Tests/BloomerTests/BloomerTests.swift
+++ Tests/BloomerTests/BloomerTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class BloomerTests: XCTestCase {
     static var allTests = [
         ("testExample", testExample),
+        ("testMany", testMany),
     ]
 }
 
@@ -19,5 +20,37 @@ extension BloomerTests {
         XCTAssert(filter.contains("yo"))
         XCTAssertFalse(filter.contains("no"))
         
+    }
+    
+    func testMany() {
+        func fibs(through: Int) -> [Int] {
+            return sequence(state: (0, 1), next: { (pair: inout (Int, Int)) -> Int? in
+                defer { pair = (pair.1, pair.0 + pair.1) }
+                return pair.1
+            }).prefix(while: { $0 <= through })
+        }
+        
+        var filter = BloomFilter<Int>(size: 1000000, hashCount: 6)
+        
+        let max = 100_000
+        let fibNumbers = fibs(through: max)
+        
+        let nonFibs: [Int] = {
+            let fibSet = Set(fibNumbers)
+            return (0...max).filter { !fibSet.contains($0) }
+        }()
+        
+        
+        fibNumbers.forEach {
+            filter.add($0)
+        }
+        
+        fibNumbers.forEach {
+            XCTAssert(filter.contains($0))
+        }
+        
+        nonFibs.forEach {
+            XCTAssertFalse(filter.contains($0))
+        }
     }
 }
